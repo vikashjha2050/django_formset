f86fdfa7e96c46acc4dc68c20fdcafe5cb877856
vikashjha <vikashjha2050@gmail.com>
2019-01-03 01:06:30 +0530

    more changes

--- django_formset/exams/views.py
+++ django_formset/exams/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.shortcuts import redirect
 
 from exams.models import exams
 from exams.forms import ExamForm
@@ -9,10 +10,11 @@ class dashboard(ListView):
 	model = exams
 	template_name = 'exams/exam_dashboard.html'
 
-def exam_add_edit(request):
+def exam_add_edit(request, pk=None):
 	exform = ExamForm()
 	if request.method == 'POST':
 		exform = ExamForm(request.POST)
 		if exform.is_valid():
 			exform.save()
+			return redirect('/exams/dashboard/')
 	return render(request, 'exams/exam_create.html',{'exform':exform})
