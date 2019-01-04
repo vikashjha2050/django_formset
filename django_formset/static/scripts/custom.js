234ad4927f35c6dcf5c8c7673da86b7c93580cc9
vikasjha <vikashjha2050@gmail.com>
2019-01-04 18:47:21 +0530

    more changes

--- django_formset/static/scripts/custom.js
+++ django_formset/static/scripts/custom.js
@@ -13,8 +13,30 @@ $("#add_form").click(function(){
         url: '/exams/getset/',
         data : {extra:extra,pk:pk},
     })
-    .done(function(response) { 
-        $('.setclass').html(response);
+    .done(function(response) {
+        $('.setclass').append(response);
         $('.man_class').find('#id_form-TOTAL_FORMS').val(total)
     });
+});
+
+$("#remove_form").click(function(){
+   var total = $('.man_class').find('#id_form-TOTAL_FORMS').val();
+   var init = $('.man_class').find('#id_form-INITIAL_FORMS').val();
+   var extra = total - init;
+   if(total>1){
+    var total = parseInt(total, 10)-1;
+    $('.man_class').find('#id_form-TOTAL_FORMS').val(total);
+    var init = parseInt(init, 10)-1;
+    if(extra==0){
+      $('.man_class').find('#id_form-INITIAL_FORMS').val(init);
+    }
+    var sub_id = $(this).parent().find('.setelement').last().find('.sub_id').html();
+    $(this).parent().find('.setelement').last().remove();
+    $.ajax({
+        url: '/exams/'+sub_id+/sub_delete/,
+    })
+    .done(function(response) {
+       alert(response);
+    });   
+  }
 });
